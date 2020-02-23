9d480ce57ef29881d3acf51863e37310294e4c61
Juan Camilo Pulido <jc.pulido@uniandes.edu.co>
2020-02-23 16:51:51 -0500

    Initial Commit

---
+++ BenchmarkML/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  BenchmarkML
+//
+//  Created by Admin on 2/23/20.
+//  Copyright © 2020 Universidad de Los Andes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
